6b9111516d36aec4b32df537129860bb3d258587
vitalielazari <vitalie.lazari@gmail.com>
2018-04-20 09:27:30 +0200

    exercise 3 & 4

--- src/com/sckeedoo/certification/Problem3.java
+++ src/com/sckeedoo/certification/Problem3.java
@@ -1,11 +1,24 @@
 package com.sckeedoo.certification;
 
+import java.util.Scanner;
+
 public class Problem3 {
-    /*
-         Write a Java method to count all vowels in a string.
-         Method should take a String as argument, and return an Integer;
-     */
+	/*
+    Write a Java program to convert minutes into a number of years and days.
+    Test Data
+    Input the number of minutes: 3456789
+    Expected Output :
+    3456789 minutes is approximately 6 years and 210 days
+	*/
     public static void main(String[] args) {
+    	System.out.println("Input the number of minutes: ");
+    	Scanner input = new Scanner(System.in);
+    	final int minutes = input.nextInt(), day = 60*24, year = day*365;
+    	input.close();
+    	 
+    	int years = minutes/year;
+    	int days = (minutes-years*year)/day;
+    	System.out.println(minutes + " is approximately " + years+ " years and " + days + " days");
 
     }
 }
---
+++ src/com/sckeedoo/certification/Problem4.java
@@ -0,0 +1,51 @@
+package com.sckeedoo.certification;
+
+import java.util.ArrayList;
+
+public class Problem4 {
+	/*
+    Write a method that takes an array as parameter and returns another
+    array without duplicate elements
+
+    Input:
+    {1,3,4,1,4,5}
+    Return:
+    {1,3,4,5}
+	 */
+    public static void main(String[] args) {
+    	int [] arrayOfInt = {1,3,4,1,4,5};
+    	ArrayList<Integer> array = new ArrayList<>();
+    	for (int i = 0; i < arrayOfInt.length; i++) {
+			array.add(arrayOfInt[i]);
+		}
+    	System.out.println(clearDuplicates(array));
+    }
+    public static String clearDuplicates(ArrayList<Integer> array) {
+    	ArrayList<Integer> newArray = new ArrayList<Integer>();
+    	
+    	for(int i = 0; i < array.size();i++) {
+    		boolean exist = exist(array.get(i), newArray);
+    		
+    		if (exist==false)  {  				
+    				newArray.add(array.get(i));
+    		}
+    		
+    	}
+    	StringBuilder sb = new StringBuilder();
+    	for(int i = 0; i< newArray.size(); i++) {
+    		if(i>0) 
+    			sb.append(',');
+    		sb.append(newArray.get(i));
+    	}
+    	return sb.toString();
+    }
+
+    public static boolean exist(Integer number, ArrayList<Integer> array) {
+    	boolean exist = false;
+    	for (int i : array) {
+			if (i == number)
+				exist = true;
+		}
+    	return exist;
+    }
+}
